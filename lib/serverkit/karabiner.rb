542edf34127c6c157963319ef6d52f7fecb0f4af
Ryo Nakamura <r7kamura@gmail.com>
2015-04-11 01:22:17 +0900

    bundle gem serverkit-karabiner

---
+++ lib/serverkit/karabiner.rb
@@ -0,0 +1,7 @@
+require "serverkit/karabiner/version"
+
+module Serverkit
+  module Karabiner
+    # Your code goes here...
+  end
+end
---
+++ lib/serverkit/karabiner/version.rb
@@ -0,0 +1,5 @@
+module Serverkit
+  module Karabiner
+    VERSION = "0.1.0"
+  end
+end
